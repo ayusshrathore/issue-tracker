eebadd5fc32418afe2c449fcba1599b09d628223
ayusshrathore <ayusshrathore@gmail.com>
2023-10-10 21:17:17 +0530

    feat: add create issue api

---
+++ app/api/issues/route.ts
@@ -0,0 +1,27 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const schema = z.object({
+	title: z.string().min(1).max(255),
+	description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+	const body = await request.json();
+
+	const validation = schema.safeParse(body);
+
+	if (!validation.success) {
+		return NextResponse.json(validation.error.errors, { status: 400 });
+	}
+
+	const newIssue = await prisma.issue.create({
+		data: {
+			title: body.title,
+			description: body.description,
+		},
+	});
+
+	return NextResponse.json(newIssue, { status: 201 });
+}
