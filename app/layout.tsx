30973109b3aab7da16e468e1cf19b226906a0822
ayusshrathore <ayusshrathore@gmail.com>
2023-10-10 21:21:37 +0530

    feat: setup `@radix-ui`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
@@ -18,8 +20,10 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<NavBar />
-				<main>{children}</main>
+				<Theme>
+					<NavBar />
+					<main>{children}</main>
+				</Theme>
 			</body>
 		</html>
 	);
