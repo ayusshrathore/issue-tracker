7ebd43dab1d732834f392c042c9d7a66cd6c41f3
ayusshrathore <ayusshrathore@gmail.com>
2023-10-15 11:49:05 +0530

    feat: add issue & loading pages

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import delay from "delay";
+import Link from "next/link";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import IssueActions from "./IssueActions";
 
@@ -27,7 +28,7 @@ const IssuesPage = async () => {
 					{issues.map((issue) => (
 						<Table.Row key={issue.id}>
 							<Table.Cell>
-								{issue.title}
+								<Link href={`/issues/${issue.id}`}>{issue.title}</Link>
 								<div className="block md:hidden">
 									<IssueStatusBadge status={issue.status} />
 								</div>
