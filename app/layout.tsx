3e48f293b344aef142afa3731ae88d4ac9c76a18
ayusshrathore <ayusshrathore@gmail.com>
2023-10-15 19:52:13 +0530

    feat: add auth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
+import AuthProvider from "./auth/Provider";
 import "./globals.css";
 import "./theme-config.css";
 
@@ -24,12 +25,14 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.variable}>
-				<Theme appearance="light" accentColor="violet">
-					<NavBar />
-					<main className="p-5">
-						<Container>{children}</Container>
-					</main>
-				</Theme>
+				<AuthProvider>
+					<Theme appearance="light" accentColor="violet">
+						<NavBar />
+						<main className="p-5">
+							<Container>{children}</Container>
+						</main>
+					</Theme>
+				</AuthProvider>
 			</body>
 		</html>
 	);
