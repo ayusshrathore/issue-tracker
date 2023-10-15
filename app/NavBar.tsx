248d09397a3a6bf16e04296d0e042a602b9b3448
ayusshrathore <ayusshrathore@gmail.com>
2023-10-15 19:52:19 +0530

    chore: add navbar layout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { Box, Container, Flex } from "@radix-ui/themes";
 import clsx from "clsx";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
 	const currentPath = usePathname();
+	const { status, data: session } = useSession();
 
 	const links = [
 		{ label: "Dashboard", href: "/" },
@@ -14,25 +17,57 @@ const NavBar = () => {
 	];
 
 	return (
-		<nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-			<Link href="/">
-				<AiFillBug className="text-xl" />
-			</Link>
-			<ul className="flex space-x-6">
-				{links.map((link) => (
-					<li key={link.href}>
-						<Link
-							className={clsx(
-								"text-zinc-500 hover:text-zinc-800 transition-colors",
-								currentPath === link.href && "text-zinc-800 font-semibold"
-							)}
-							href={link.href}
-						>
-							{link.label}
+		<nav className="border-b mb-5 px-5 py-3 items-center">
+			<Container>
+				<Flex justify="between">
+					<Flex align="center" gap="3">
+						<Link href="/">
+							<AiFillBug className="text-xl" />
 						</Link>
-					</li>
-				))}
-			</ul>
+						<ul className="flex space-x-6">
+							{links.map((link) => (
+								<li key={link.href}>
+									<Link
+										className={clsx(
+											"text-zinc-500 hover:text-zinc-800 transition-colors",
+											currentPath === link.href && "text-zinc-800 font-semibold"
+										)}
+										href={link.href}
+									>
+										{link.label}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</Flex>
+					<Box>
+						{status === "authenticated" && (
+							<div className="flex items-center space-x-4 ml-auto">
+								<span className="text-zinc-500">{session?.user?.name}</span>
+								<img
+									className="w-10 h-10 rounded-full"
+									src={session?.user?.image!}
+									alt={session?.user?.name!}
+								/>
+								<Link
+									href="/api/auth/signout"
+									className="text-zinc-500 hover:text-zinc-800 transition-colors"
+								>
+									Log out
+								</Link>
+							</div>
+						)}
+						{status === "unauthenticated" && (
+							<Link
+								href="/api/auth/signin"
+								className="text-zinc-500 hover:text-zinc-800 transition-colors"
+							>
+								Log in
+							</Link>
+						)}
+					</Box>
+				</Flex>
+			</Container>
 		</nav>
 	);
 };
