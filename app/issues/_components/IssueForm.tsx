4d22b115e89f25a26e2ce4f6c4898af5f9ad0f57
ayusshrathore <ayusshrathore@gmail.com>
2023-10-15 18:06:01 +0530

    fix: caching issue

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -36,6 +36,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
 			if (issue) await axios.patch(`/api/issues/${issue.id}`, data);
 			else await axios.post("/api/issues", data);
 			router.push("/issues");
+			router.refresh();
 		} catch (error) {
 			setError("An error occurred while creating the issue.");
 		}
