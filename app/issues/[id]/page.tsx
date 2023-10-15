c3cb2250f252a89e4c7a8a8982784e00641e0edf
ayusshrathore <ayusshrathore@gmail.com>
2023-10-15 11:54:56 +0530

    chore: update issue page styles

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 
 interface Props {
@@ -7,12 +9,13 @@ interface Props {
 	};
 }
 
-const IssueDetailPage = async ({ params: { id } }: Props) => {
+const IssueDetailPage = async ({ params }: Props) => {
+	const id = parseInt(params.id);
 	if (typeof id !== "number") notFound();
 
 	const issue = await prisma.issue.findUnique({
 		where: {
-			id: parseInt(id),
+			id,
 		},
 	});
 
@@ -20,10 +23,14 @@ const IssueDetailPage = async ({ params: { id } }: Props) => {
 
 	return (
 		<div>
-			<p>{issue.title}</p>
-			<p>{issue.description}</p>
-			<p>{issue.status}</p>
-			<p>{issue.createdAt.toDateString()}</p>
+			<Heading>{issue.title}</Heading>
+			<Flex className="space-x-3" my="2">
+				<IssueStatusBadge status={issue.status} />
+				<Text>{issue.createdAt.toDateString()}</Text>
+			</Flex>
+			<Card>
+				<p>{issue.description}</p>
+			</Card>
 		</div>
 	);
 };
