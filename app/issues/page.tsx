416fd6cebaf33af8588e466d26dc9563e76c565a
ayusshrathore <ayusshrathore@gmail.com>
2023-10-15 12:55:26 +0530

    feat: add link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,8 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import delay from "delay";
-import Link from "next/link";
 import IssueStatusBadge from "../components/IssueStatusBadge";
+import Link from "../components/Link";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
